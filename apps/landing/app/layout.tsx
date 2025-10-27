ed425cd6a522f959f7e12cef89d064196adf1d3f
Moeen Mahmud <write2fahim007@gmail.com>
2025-10-27 12:16:14 +0600

    chore(@osmynt-core/landing): updates og image

--- apps/landing/app/layout.tsx
+++ apps/landing/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { Suspense } from "react";
 import { StructuredData } from "@/components/structured-data";
@@ -99,6 +100,7 @@ export default function RootLayout({
 					<PerformanceOptimizer />
 					{/* <RebrandBanner /> */}
 					<Suspense fallback={null}>{children}</Suspense>
+					<SpeedInsights />
 					<Analytics />
 				</ThemeProvider>
 			</body>
